d8eedce43a1fd80cada3b96c5b1146d844a65887
Prathamesh Bhongale <83685441+Prathamesh8805@users.noreply.github.com>
2022-11-19 09:35:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "ap-northeast-1_HLJhPiQ74", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7rk5uhechkg3jusa67m3vgsni2", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
